42d2aa97f85e6289750db0120c85f3c1f69ed8fe
Rudy Amaral <rudy.amaral@gmail.com>
2016-06-21 15:28:13 +0200

    Fusion des pages web

--- web/dernieres24h.php
+++ web/dernieres24h.php
@@ -1,6 +1,13 @@
 <!DOCTYPE html>
 <?php
 	session_start();
+		include("../modules/connexionBDD.php");
+	$heurodatage = date('Y-m-d H:i:s');
+	$heurodatage24H = date('Y-m-d H:i:s', mktime(date('H')-24, date('i'), date('s'), date('m'), date('d'), date('Y')));
+	$reponse = $bdd->query('SELECT *
+							FROM Mesures 
+							WHERE Heurodatage BETWEEN \'' . $heurodatage24H . '\' AND \'' . $heurodatage . '\'
+							AND Id_Pieces = 1');
 ?>
 <html>
     <head>
@@ -27,8 +34,114 @@
 					<?php include('menu.php'); ?>
 				</td>
 				<td align="center">
+					<img src="/img/vide.png" height="50">
+					<table>
+						<tr>
+							<td class="cadre">
+								<table>
+									<tr>
+										<td colspan="5" align="center">
+											<table>
+												<tr>
+													<td><img src="/img/bebe.png" height="64"></td>
+													<td>Chambre</td>
+												</tr>
+											</table>
+										</td>
+									</tr>
+									<tr>
+										<td class="tableau">Heurodatage</td>
+										<td class="tableau">Temp. Int</td>
+										<td class="tableau">Temp. Ext</td>
+										<td class="tableau">Humidite</td>
+										<td class="tableau">Radiateur</td>
+									</tr>
+									<?php 
+										while ($donnees = $reponse->fetch()) {
+									?>
+									<tr class="tableau">
+										<td class="tableau" align="center">
+											<?php echo $donnees['Heurodatage'];; ?>
+										</td>
+										<td class="tableau" align="center">
+											<?php echo (int)$donnees['Tempint'];; ?>
+										</td>
+										<td class="tableau" align="center">
+											<?php echo (int)$donnees['Tempext'];; ?>
+										</td>
+										<td class="tableau" align="center">
+											<?php echo (int)$donnees['Humidite'];; ?>
+										</td>
+										<td class="tableau" align="center">
+											<?php echo (int)$donnees['Radiateur'];; ?>
+										</td>
+									</tr>
+									<?php 
+										}
+										$reponse->closeCursor(); 
+									?>
+								</table>
+							</td>
+						</tr>
+					</table>
+					<img src="/img/vide.png" height="50">
 					<?php
-						include('dernieres24h-code.php');
+						$reponse = $bdd->query('SELECT *
+												FROM Mesures 
+												WHERE Heurodatage BETWEEN \'' . $heurodatage24H . '\' AND \'' . $heurodatage . '\'
+												AND Id_Pieces = 2');
+					?>
+					<table>
+						<tr>
+							<td class="cadre">
+								<table>
+									<tr>
+										<td colspan="5" align="center">
+											<table>
+												<tr>
+													<td><img src="/img/salon.png" width="64"></td>
+													<td>Salon</td>
+												</tr>
+											</table>
+										</td>
+									</tr>
+									<tr>
+										<td class="tableau">Heurodatage</td>
+										<td class="tableau">Temp. Int</td>
+										<td class="tableau">Temp. Ext</td>
+										<td class="tableau">Humidite</td>
+										<td class="tableau">Radiateur</td>
+									</tr>
+									<?php 
+										while ($donnees = $reponse->fetch()) {
+									?>
+									<tr class="tableau">
+										<td class="tableau" align="center">
+											<?php echo $donnees['Heurodatage'];; ?>
+										</td>
+										<td class="tableau" align="center">
+											<?php echo (int)$donnees['Tempint'];; ?>
+										</td>
+										<td class="tableau" align="center">
+											<?php echo (int)$donnees['Tempext'];; ?>
+										</td>
+										<td class="tableau" align="center">
+											<?php echo (int)$donnees['Humidite'];; ?>
+										</td>
+										<td class="tableau" align="center">
+											<?php echo (int)$donnees['Radiateur'];; ?>
+										</td>
+									</tr>
+									<?php 
+										}
+										$reponse->closeCursor(); 
+									?>
+								</table>
+							</td>
+						</tr>
+					</table>
+
+					<?php
 						}
 						else
 						{
--- web/etat.php
+++ web/etat.php
@@ -2,6 +2,17 @@
 <?php
 	session_start();
 	include('../lib/simple_html_dom.php');
+	function ping($ip_a_tester)
+	{
+			if(exec("ping ".$ip_a_tester." -w 1"))
+		{
+			return "on";
+		}
+		else
+		{
+			return "off";
+		}
+	}
 ?>
 <html>
     <head>
@@ -10,6 +21,7 @@
         <title>ASGARD - Etat</title>
     </head>
     <body>
+		
 		<table class="page">
 			<tr align="center">
 				<td>
@@ -28,8 +40,162 @@
 					<?php include('menu.php'); ?>
 				</td>
 				<td align="center">
+					<img src="/img/vide.png" height="50">
+					<table>
+						<tr>
+							<td class="cadre">
+								<table>
+									<tr>
+										<td align="center" class="tableau">
+											Objet
+										</td>
+										<td align="center" class="tableau">
+											Connexion
+										</td>
+										<td align="center" class="tableau">
+											Temperature
+										</td>
+									</tr>
+									<tr>
+										<td align="center" class="tableau">
+											<img src="/img/raspberryon.png" title="WLAN : 192.168.1.22"><br>
+											GIT
+										</td>
+										<td align="center" class="tableau">
+											<?php 
+												$connec = ping('192.168.1.22');
+												echo $connec;
+											?>
+										</td>
+										<td align="center" class="tableau">
+											<?php
+												if($connec == 'on')
+												{
+													$html = file_get_html('http://192.168.1.22/temppi.php');
+													foreach($html->find('input[name=temperature]') as $element) 
+													$temperature=$element->value;
+													echo $temperature;
+												}
+												else
+												{
+													echo 'NA';
+												}
+											?>
+										</td>
+									</tr>
+									<tr>
+										<td align="center" class="tableau">
+											<a href="brain.php"><img src="/img/raspberryon.png" title="LAN : 192.168.1.16 WLAN : 192.168.1.17"><br>
+											Brain</a>
+										</td>
+										<td align="center" class="tableau">
+											<?php 
+												$connec = ping('192.168.1.16');
+												echo $connec;
+											?>
+										</td>
+										<td align="center" class="tableau">
+											<?php
+												if($connec == 'on')
+												{
+													$html = file_get_html('http://192.168.1.16/script/temppi.php');
+													foreach($html->find('input[name=temperature]') as $element) 
+														$temperature=$element->value;
+													echo $temperature;
+												}
+												else
+												{
+													echo 'NA';
+												}
+											?>
+										</td>
+									</tr>
+									<tr>
+										<td align="center" class="tableau">
+											<img src="/img/raspberryon.png" title="WLAN : 192.168.1.19"><br>
+											Meteo
+										</td>
+										<td align="center" class="tableau">
+											<?php 
+												$connec = ping('192.168.1.19');
+												echo $connec;
+											?>
+										</td>
+										<td align="center" class="tableau">
+											<?php
+												if($connec == 'on')
+												{
+													$html = file_get_html('http://192.168.1.19/temppi.php');
+													foreach($html->find('input[name=temperature]') as $element) 
+														$temperature=$element->value;
+													echo $temperature;
+												}
+												else
+												{
+													echo 'NA';
+												}
+											?>
+										</td>
+									</tr>
+									<tr>
+										<td align="center" class="tableau">
+											<img src="/img/sondetempon.png" title="WLAN : 192.168.1.15"><br>
+											S.Chambre
+										</td>
+										<td align="center" class="tableau">
+											<?php 
+												$connec = ping('192.168.1.15');
+												echo $connec;
+											?>
+										</td>
+										<td align="center" class="tableau">
+											<?php
+												if($connec == 'on')
+												{
+													$html = file_get_html('http://192.168.1.15');
+													foreach($html->find('input[name=temperature]') as $element) 
+														$temperature=$element->value;
+													echo (int)$temperature;
+												}
+												else
+												{
+													echo 'NA';
+												}
+											?>
+										</td>
+									</tr>
+									<tr>
+										<td align="center" class="tableau">
+											<img src="/img/sondetempon.png" title="WLAN : 192.168.1.21"><br>
+											S.Salon
+										</td>
+										<td align="center" class="tableau">
+											<?php 
+												$connec = ping('192.168.1.21');
+												echo $connec;
+											?>
+										</td>
+										<td align="center" class="tableau">
+											<?php
+												if($connec == 'on')
+												{
+													$html = file_get_html('http://192.168.1.21');
+													foreach($html->find('input[name=temperature]') as $element) 
+														$temperature=$element->value;
+													echo (int)$temperature;
+												}
+												else
+												{
+													echo 'NA';
+												}
+											?>
+										</td>
+									</tr>
+								</table>
+							</td>
+						</tr>
+					</table>
 					<?php
-						include('etat-code.php');
 						}
 						else
 						{
