d4d43d3effc9634f5b54256b042c0246058f08bb
belkeen <rudy.amaral@gmail.com>
2016-07-12 13:08:31 +0200

    Nettoyage temppi

--- script/temppi.php
+++
@@ -1,8 +0,0 @@
-<!/usr/bin/php>
-<?php
-        exec('sudo /opt/vc/bin/vcgencmd measure_temp', $reponse);
-        $temppi = substr($reponse[0], 5, 2);
-?>
-<form>
-        <input type="text" name="temperature" value="<?php echo $temppi; ?>" />
-</form>
