f3ab4376168c8c8db7b56dededcd5a2eb30f90c3
belkeen <rudy.amaral@gmail.com>
2016-10-10 15:21:03 +0200

    Classement + recent logs

--- Odin/sol/meteo.php
+++ Odin/sol/meteo.php
@@ -213,7 +213,7 @@
 											WHERE Logs.Id_Codes = Codes.Id
 											AND Codes.Id_Equipements = Equipements.Id
 											AND (Codes.Id > 200 AND Codes.Id < 300)
-											ORDER BY Logs.Heurodatage');
+											ORDER BY Logs.Heurodatage DESC');
 				while($infos_log = $logs_BDD->fetch()) {
 					if($infos_log['Warning']) {
 						$warning = 'KO';
