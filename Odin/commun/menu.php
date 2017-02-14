b7471c5e1311b34ee53ddb731180269f75edb584
Rudy Amaral <rudy.amaral@gmail.com>
2017-02-14 17:24:55 +0100

    Suppression du bébé phone

--- Odin/commun/menu.php
+++ Odin/commun/menu.php
@@ -2,5 +2,4 @@
 <a href="/Odin/odin.php"><div class="ongletmenu<?php if($page == 'odin') { echo 'selected'; } ?>"><div class="textemenu">Odin</div></div></a>
 <a href="/Odin/sol.php"><div class="ongletmenu<?php if($page == 'sol') { echo 'selected'; } ?>"><div class="textemenu">Sol</div></div></a>
 <a href="/Odin/fimafeng.php"><div class="ongletmenu<?php if($page == 'fimafeng') { echo 'selected'; } ?>"><div class="textemenu">Fimafeng</div></div></a>
-<a href="/Odin/frigg/view.php"><div class="ongletmenu<?php if($page == 'frigg') { echo 'selected'; } ?>"><div class="textemenu">Frigg</div></div></a>
 <div class="right1pct"></div>
