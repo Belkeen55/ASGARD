cb36264881b13692ab3464c5e6d0f1b2642ec1cf
Rudy Amaral <rudy.amaral@gmail.com>
2017-04-12 10:44:02 +0200

    Modification menu et ajout bot

--- Odin/commun/menu.php
+++ Odin/commun/menu.php
@@ -1,5 +1,5 @@
 <div class="left1pct"></div>
-<a href="/Odin/odin.php"><div class="ongletmenu<?php if($page == 'odin') { echo 'selected'; } ?>"><div class="textemenu">Odin</div></div></a>
-<a href="/Odin/sol.php"><div class="ongletmenu<?php if($page == 'sol') { echo 'selected'; } ?>"><div class="textemenu">Sol</div></div></a>
-<a href="/Odin/fimafeng.php"><div class="ongletmenu<?php if($page == 'fimafeng') { echo 'selected'; } ?>"><div class="textemenu">Fimafeng</div></div></a>
+<a href="/Odin/odin.php"><div class="ongletmenu<?php if($page == 'odin') { echo 'selected'; } ?>"><div class="textemenu">Applis</div></div></a>
+<a href="/Odin/sol.php"><div class="ongletmenu<?php if($page == 'sol') { echo 'selected'; } ?>"><div class="textemenu">Domo</div></div></a>
+<a href="/Odin/fimafeng.php"><div class="ongletmenu<?php if($page == 'fimafeng') { echo 'selected'; } ?>"><div class="textemenu">Sys</div></div></a>
 <div class="right1pct"></div>
---
+++ Telegram/readme.md
@@ -0,0 +1,12 @@
+Installation TelegramBot
+==
+#### Installation de composer
+	cd /usr/src 
+	sudo apt-get install curl php5-cli 
+	curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer 
+
+#### Installation des packages
+	sudo apt install php5-curl 
+	
+#### Installation de TelegramBot
+	sudo composer require longman/telegram-bot 
